82d6dc9079051f132b2035369f0913ccbaf16558
Dang Le Hai <haidangle678@gmail.com>
2024-09-16 22:28:53 +0700

    Update models

--- document/models.py
+++ document/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self) -> str:
+        return self.title
